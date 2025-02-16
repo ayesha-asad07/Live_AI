4fdbf85b15727715dbb95b7cfa42b75940a8ecf5
Ayesha Virk <ashiasad546@gmail.com>
2025-02-16 13:09:48 +0500

    Updated chatApi to use environment variables

--- utils/chatApi.js
+++ utils/chatApi.js
@@ -1,11 +1,17 @@
+const API_URL = process.env.NEXT_PUBLIC_CHATBOT_API_URL || "http://localhost:5000";
+
 export async function getChatbotResponse(userMessage) {
-    const response = await fetch("http://localhost:5000/chat", {
+    const response = await fetch(`${API_URL}/chat`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({ userMessage }),
     });
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch chatbot response");
+    }
 
     const data = await response.json();
     return data.reply;
