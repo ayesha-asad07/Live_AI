c4d9924b496697b9c84cebc73502fe5a6d165194
Ayesha Virk <ashiasad546@gmail.com>
2025-02-17 21:25:50 +0500

    updated

--- utils/chatApi.js
+++ utils/chatApi.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_CHATBOT_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_CHATBOT_API_URL ;
 
 export async function getChatbotResponse(userMessage) {
     const response = await fetch(`${API_URL}/chat`, {
